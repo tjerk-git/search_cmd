3442610500792c9de2c4346c3362dfe5a453ee5e
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2022-01-12 22:02:03 +0100

    domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.action_mailer.default_url_options = { host: 'https://play.cmdlwd.nl' }
+  config.action_mailer.default_url_options = { host: 'https://searchcmd.herokuapp.com' }
   config.action_mailer.delivery_method = :postmark
 
   config.action_mailer.postmark_settings = {
